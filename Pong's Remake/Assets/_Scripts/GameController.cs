1f5666b9ccf1d4620ce978522ff39c1fc24d86ef
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2020-06-27 21:23:18 -0300

    Project Creation. Player movement set

---
+++ Pong's Remake/Assets/_Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //Camera Boundaries:
+    public float minX, maxX, minY, maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void MeasureScreen()
+    {
+        float camDistance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Vector2 bottomCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
+        Vector2 topCorner = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
+
+        minX = bottomCorner.x;
+        maxX = topCorner.x;
+        minY = bottomCorner.y;
+        maxY = topCorner.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeasureScreen();
+    }
+}
---
+++ Pong's Remake/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Movement Values
+    [SerializeField] float speed = 1f;
+    [SerializeField] Vector2 input;
+    [SerializeField] Vector2 position;
+    //Player Values && Components
+    [SerializeField] float halfHeightPlayer;
+    Rigidbody2D rbPlayer;
+    SpriteRenderer spriteRendererPlayer;
+    [SerializeField] GameObject gameControllerGO;
+    [SerializeField] GameController gameControllerScript;
+    enum State {Inside, BoundarieUp, BoundarieDown};
+    State state = State.Inside;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponents();
+    }
+    void GetComponents()
+    {
+        rbPlayer = gameObject.GetComponent<Rigidbody2D>();
+        gameControllerGO = FindObjectOfType<GameController>().gameObject;
+        gameControllerScript = gameControllerGO.GetComponent<GameController>();
+        spriteRendererPlayer = gameObject.GetComponent<SpriteRenderer>();
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputToMove();
+        MeasurePlayer();
+        CheckBoundaries();
+    }
+    void GetInputToMove()
+    {
+        if (gameObject.CompareTag("Player1"))
+        {
+            input.y = Input.GetAxisRaw("Vertical_P1");
+            position = transform.position;
+        }
+        if (gameObject.CompareTag("Player2"))
+        {
+            input.y = Input.GetAxisRaw("Vertical_P2");
+            position = transform.position;
+        }
+    }
+    void MeasurePlayer()
+    {
+        float offset = 0.5f;
+        halfHeightPlayer = spriteRendererPlayer.bounds.size.y/2 + offset;
+    }
+    void CheckBoundaries()
+    {
+        if (position.y - halfHeightPlayer > gameControllerScript.minY && position.y + halfHeightPlayer < gameControllerScript.maxY)
+        {
+            state = State.Inside;
+        }
+        if (position.y - halfHeightPlayer <= gameControllerScript.minY )
+        {
+            state = State.BoundarieDown;
+        }
+        if (position.y + halfHeightPlayer >= gameControllerScript.maxY)
+        {
+            state = State.BoundarieUp;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        if (state == State.Inside)
+        {
+            rbPlayer.MovePosition(position + input * speed);
+        }
+        if (state == State.BoundarieUp)
+        {
+            if (input.y == 1)
+            {
+
+            }
+            else
+                rbPlayer.MovePosition(position + input * speed);
+        }
+        if (state == State.BoundarieDown)
+        {
+            if (input.y == -1)
+            {
+
+            }
+            else
+                rbPlayer.MovePosition(position + input * speed);
+        }
+    }
+
+}
